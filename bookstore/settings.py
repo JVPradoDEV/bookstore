8e0b0c65291b01f648722214338ac3f027f10671
JVPradoDev <joaovitordoprado15@hotmail.com>
2026-09-22 19:10:43 -0300

    commit vazio

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 USE_L1ON = True
 
 
-# Static files (CSS, JavaScript, Images)
+## Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
